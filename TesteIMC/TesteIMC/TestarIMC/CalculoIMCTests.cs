a5ba95ca1f0ba2982164b26882b0c36964c80507
Nilknarf <backendfranklin@gmail.com>
2022-11-12 23:07:30 +0000

    Atividade Online 2

---
+++ TesteIMC/TesteIMC/TestarIMC/CalculoIMCTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TesteIMC;
+
+namespace TestarIMC
+{
+    //classe de testes que voce queira executar
+    [TestClass]
+    public class CalculoIMCTests
+    {
+        //método de teste
+        [TestMethod]
+        public void CalcularIMC()
+        {
+            //ARRANGE - organizar e preparar o teste
+            double peso = 110;
+            double altura = 1.79;
+
+            //ACT - agir - execução/chamada do método
+            var resultadoIMC = Operacoes.CalcularIMC(peso, altura);
+
+            //ASSERT - comportamento esperado , comportamento obtido
+            Assert.AreEqual(34.33, resultadoIMC);
+        }
+    }
+
+}
---
+++ TesteIMC/TesteIMC/TesteIMCXxunit/ClassificarIMCTests.cs
@@ -0,0 +1,31 @@
+﻿using TestarIMC;
+using TesteIMC;
+using Xunit;
+
+namespace TesteIMCXunit
+{
+    public class ClassificarIMCTests
+    {
+        //método de teste
+        [Fact]
+        //descricao do teste
+        public void ClassificarIMC_RetornaResultado()
+        {
+            //ARRANGE
+            double imc = 28;
+            //ACT
+            var classificacaoIMC = Operacoes.ClassificarIMC(imc);
+            //ASSERT
+            Assert.Equal("Sobrepeso", classificacaoIMC);
+        }
+
+        [Theory]
+        [InlineData(28, "Sobrepeso")]
+        [InlineData(31, "Obesidade Grau I")]
+        public void ClassificarIMC_RetornaResultado_ParaUmaListaDeValores(double primeiroNumero, string resultadoEsperado)
+        {
+            var resultadoDoIMC = Operacoes.ClassificarIMC(primeiroNumero);
+            Assert.Equal(resultadoEsperado, resultadoDoIMC);
+        }
+    }
+}
